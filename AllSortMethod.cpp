2195f23a210c7f88b1986c31510e9ba5f4033c7f
PrajneshBaliga <65158099+PrajneshBaliga@users.noreply.github.com>
2026-02-02 10:51:55 +0530

    Update AllSortMethod.cpp

--- AllSortMethod.cpp
+++ AllSortMethod.cpp
@@ -1,5 +1,66 @@
 //all sorting methods 
 #include <stdio.h>
+#include <stdlib.h>
+
+void merge(int arr[], int l, int m, int r){
+    
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r){
+    
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
 
 void swap(int* xp, int* yp){
     int temp = *xp;
@@ -8,6 +69,7 @@ void swap(int* xp, int* yp){
 }
 
 void bubbleSort(int arr[], int n){
+	
     int i, j;
       for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -22,7 +84,30 @@ void bubbleSort(int arr[], int n){
             break;
     }
 }
-
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+      
+        // Assume the current position holds
+        // the minimum element
+        int min_idx = i;
+        
+        // Iterate through the unsorted portion
+        // to find the actual minimum
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+              
+                // Update min_idx if a smaller element is found
+                min_idx = j;
+            }
+        }
+        
+        // Move minimum element to its
+        // correct position
+        int temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
+}
 int partition(int arr[], int low, int high) {
     
     int pivot = arr[high];
