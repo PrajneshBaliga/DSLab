b0aa0971436f9fb8bd865aa3288c35e8a227e5cd
PrajneshBaliga <65158099+PrajneshBaliga@users.noreply.github.com>
2026-02-03 18:05:05 +0530

    Update AllSortMethod.cpp

--- AllSortMethod.cpp
+++ AllSortMethod.cpp
@@ -1,191 +1,157 @@
-//all sorting methods 
+// All Sorting Methods
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void merge(int arr[], int l, int m, int r){
-    
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
 
-    int L[n1], R[n2];
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
+//Bubble Sort 
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
         }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    // Copy the remnig elements of L[],
-    // i
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    // Copy the remaining elements of R[],
-    // if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+        if (!swapped)
+            break;
     }
 }
 
-void mergeSort(int arr[], int l, int r){
-    
+//Selection Sort
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        swap(&arr[i], &arr[min]);
+    }
+}
+
+//Insertion Sort 
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+//Merge Sort
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1, n2 = r - m;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    }
+
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int l, int r) {
     if (l < r) {
         int m = l + (r - l) / 2;
-
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
-
         merge(arr, l, m, r);
     }
 }
 
-void swap(int* xp, int* yp){
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void bubbleSort(int arr[], int n){
-	
-    int i, j;
-    bool swapped;
-      for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);
-                swapped = true;
-      bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-            }
-        }
-        if (swapped == false)
-            break;
-    }
-}
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-      
-        // Assume the current position holds
-        // the minimum element
-        int min_idx = i;
-        
-        // Iterate through the unsorted portion
-        // to find the actual minimum
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
-              
-                // Update min_idx if a smaller element is found
-                min_idx = j;
-            }
-        }
-        
-        // Move minimum element to its
-        // correct position
-        int temp = arr[i];
-        arr[i] = arr[min_idx];
-        arr[min_idx] = temp;
-    }
-}
+// Quick Sort
 int partition(int arr[], int low, int high) {
-    
     int pivot = arr[high];
     int i = low - 1;
-    for (int j = low; j <= high - 1; j++) {
+
+    for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
             swap(&arr[i], &arr[j]);
         }
     }
-    
-    swap(&arr[i + 1], &arr[high]);  
+    swap(&arr[i + 1], &arr[high]);
     return i + 1;
 }
 
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        
         int pi = partition(arr, low, high);
-
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
-void insertionSort(int arr[], int n)
-{
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-}
 int main() {
-    int a,c,d;
-	printf("Size of array:");
-    scanf("%d", &a);
-    printf("\n");
-    int arr[a];
-    for (int i = 0; i<a; i++)
-    {
-    	scanf("%d", &arr[i]);
-	}
-	do{
-	printf("chose your shorting method:");
-	printf("1.Bubble Sort");
-	printf("2.Selection Sort");
-	printf("3.Insertion Sort");
-	printf("4.Merge Sort");
-	printf("5.Quick Sort");
-	printf("6.Exit");
-	scanf("%d",&c);
-	switch(c){
-		case 1:
-			bubbleSort(arr,a);
-			break;
-		case 2:
-			selectionSort(arr,a);
-			break;
-		case 3:
-			insertionSort(arr,a);
-			break;
-		case 4:
-			mergeSort(arr,a);
-			break;
-		case 5:
-			quickSort(arr,a);
-			break;
-		case 6:
-			break;		
-	}
-	for (int i = 0; i<a; i++)
-    {
-    	printf("%d", arr[i]);
-	}
-	printf("continue?");
-	printf("1.Y,2.N");
-	scanf("%d",&d)
-}while(d!=2)
+    int n, choice, cont;
+
+    printf("Size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    do {
+        printf("\nChoose sorting method:\n");
+        printf("1. Bubble Sort\n");
+        printf("2. Selection Sort\n");
+        printf("3. Insertion Sort\n");
+        printf("4. Merge Sort\n");
+        printf("5. Quick Sort\n");
+        printf("6. Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: 
+				bubbleSort(arr, n); 
+				break;
+            case 2: 
+				selectionSort(arr, n); 
+				break;
+            case 3: 
+				insertionSort(arr, n); 
+				break;
+            case 4: 
+				mergeSort(arr, 0, n - 1); 
+				break;
+            case 5: 
+				quickSort(arr, 0, n - 1); 
+				break;
+            case 6: 
+				return 0;
+            default: 
+				printf("Invalid choice\n");
+        }
+
+        printf("Sorted array: ");
+        for (int i = 0; i < n; i++)
+            printf("%d ", arr[i]);
+
+        printf("\nContinue? (1.Yes / 2.No): ");
+        scanf("%d", &cont);
+
+    } while (cont != 2);
+
+    return 0;
 }
